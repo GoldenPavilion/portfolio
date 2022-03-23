c3b7470489126c6da87297d9d40d7e6ec93995c3
GoldenPavilion <rcorystine@gmail.com>
2022-03-23 19:08:21 -0400

    created NavBar component

---
+++ src/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NavBar = () => {
+    return(
+        <div className="navbar navbar-expand-lg navbar-dark bg-dark">
+        </div>
+    )
+}
+
+export default NavBar;
