d0742b8f10137f4456a2f9b073d48e544abeb6e7
GoldenPavilion <rcorystine@gmail.com>
2022-03-25 19:51:12 -0400

    changes title on mouse hover

--- src/Main.js
+++ src/Main.js
@@ -2,11 +2,14 @@ import React, { useState } from 'react';
 
 const Main = () => {
 
-    const [ title, setTitle ] = useState("Hello I'm Cory")
+    const [ title, setTitle ] = useState("Hi I'm Cory!")
 
     return(
         <div className="jumbotron">
-            <h1>{ title }</h1>
+            <h1
+                onMouseEnter={() => setTitle("And I Build Web Applications!")}
+                onMouseLeave={() => setTitle("Hi I'm Cory!")}
+            >{ title }</h1>
         </div>
     )
 }
