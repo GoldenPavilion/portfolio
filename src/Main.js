131e2c470064ae5ea6679f67a5ef7ad08028f068
GoldenPavilion <rcorystine@gmail.com>
2022-03-25 19:46:27 -0400

    sets title as state

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Main = () => {
+
+    const [ title, setTitle ] = useState("Hello I'm Cory")
+
     return(
         <div className="jumbotron">
-            <h1>Hello I'm Cory!</h1>
+            <h1>{ title }</h1>
         </div>
     )
 }
