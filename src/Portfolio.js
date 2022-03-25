ec6ee096f220a0cdbf5faff57962d384b360d85f
GoldenPavilion <rcorystine@gmail.com>
2022-03-25 19:42:21 -0400

    imported Main into Portfolio

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import NavBar from './NavBar.js';
+import Main from './Main.js';
 
 const Portfolio = () => {
     return(
         <div>
             <NavBar />
-            Portfolio Test
+            <Main />
         </div>
     )
 }
