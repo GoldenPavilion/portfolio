a8a5de3135eb7486ff4bc2b5ce94e826fbdc7bd9
GoldenPavilion <rcorystine@gmail.com>
2022-03-24 21:32:43 -0400

    added nav links

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,8 +2,34 @@ import React from 'react';
 
 const NavBar = () => {
     return(
-        <div className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="navbar-brand">Robert Cory Stine</div>
+        <div className="navbar navbar-default">
+            <div className="container-fluid">
+                <div className="navbar-header">    
+                    <div className="navbar-brand">Robert Cory Stine</div>
+                </div>
+                <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <p className="nav-link">Resume</p>
+                        </li>
+                        <li className="nav-item">
+                            <p className="nav-link">Projects</p>
+                        </li>
+                        <li className="nav-item">
+                            <p className="nav-link">Skills</p>
+                        </li>
+                        <li className="nav-item">
+                            <p className="nav-link">Blog</p>
+                        </li>
+                        <li className="nav-item">
+                            <p className="nav-link">Github</p>
+                        </li>
+                        <li className="nav-item">
+                           <p className="nav-link">LinkedIn</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
