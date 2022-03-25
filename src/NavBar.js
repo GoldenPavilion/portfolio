499f1b5f25c46178df92d3bec4d28a337a85317d
GoldenPavilion <rcorystine@gmail.com>
2022-03-24 21:17:00 -0400

    added navbar brand

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,6 +3,7 @@ import React from 'react';
 const NavBar = () => {
     return(
         <div className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="navbar-brand">Robert Cory Stine</div>
         </div>
     )
 }
