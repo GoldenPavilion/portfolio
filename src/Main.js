4a235105b54a80fe6520e06e0a2a56f5865fcd0d
GoldenPavilion <rcorystine@gmail.com>
2022-03-25 20:09:01 -0400

    added image to jumbotron

--- src/Main.js
+++ src/Main.js
@@ -6,10 +6,12 @@ const Main = () => {
 
     return(
         <div className="jumbotron">
+            <img src="media/Headshot2Copy.jpg" alt="A headshot of Cory!"/>
             <h1
                 onMouseEnter={() => setTitle("And I Build Web Applications!")}
                 onMouseLeave={() => setTitle("Hi I'm Cory!")}
             >{ title }</h1>
+            <p>I'm a full stack web developer, a customer service expert, and a big ol' nerd.</p>
         </div>
     )
 }
