f0817ef1de6ea858d5b4a8ff801d83de37b3a1b1
GoldenPavilion <rcorystine@gmail.com>
2022-03-28 20:23:17 -0400

    added logos to TechStack

--- src/TechStack.js
+++ src/TechStack.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 const TechStack = () => {
     return(
-        <div>
+        <div className="tech-stack">
+            <i class="devicon-javascript-plain"></i>
+            <i class="devicon-react-original-wordmark colored"></i>
+            <i class="devicon-redux-original colored"></i>
+            <i class="devicon-ruby-plain-wordmark colored"></i>
+            <i class="devicon-rails-plain-wordmark colored"></i>
+            <i class="devicon-html5-plain-wordmark"></i>
+            <i class="devicon-css3-plain-wordmark colored"></i>
+            <i class="devicon-bootstrap-plain-wordmark"></i>
+            <i class="devicon-npm-original-wordmark"></i>
+            <i class="devicon-fastapi-plain-wordmark colored"></i>
+            <i class="devicon-postgresql-plain-wordmark colored"></i>
+            <i class="devicon-sqlite-plain-wordmark colored"></i>
+            <i class="devicon-webpack-plain-wordmark colored"></i>
+            <i class="devicon-github-original-wordmark colored"></i>
+            <i class="devicon-vscode-plain-wordmark colored"></i>
+            <i class="devicon-slack-plain-wordmark colored"></i>
+            <i class="devicon-jira-plain-wordmark colored"></i>
+            <i class="devicon-trello-plain-wordmark colored"></i>
+            <i class="devicon-csharp-line-wordmark"></i>
+            <i class="devicon-unity-original-wordmark colored"></i>
         </div>
     )
 }
