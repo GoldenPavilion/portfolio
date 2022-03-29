1625e1bb3330ba31c354549d7f4d9afd21a7efda
GoldenPavilion <rcorystine@gmail.com>
2022-03-28 20:25:07 -0400

    imported TechStack into Portfolio

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from './NavBar.js';
 import Main from './Main.js';
+import TechStack from './TechStack.js';
 
 const Portfolio = () => {
     return(
         <div>
             <NavBar />
             <Main />
+            <TechStack />
         </div>
     )
 }
