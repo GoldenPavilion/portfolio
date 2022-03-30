5787f4ce4c3c44884d77211dc98efaf9a0db0f9b
GoldenPavilion <rcorystine@gmail.com>
2022-03-29 21:35:17 -0400

    set up Links in NavBar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,31 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return(
         <div className="navbar navbar-default">
             <div className="container-fluid">
                 <div className="navbar-header">    
-                    <div className="navbar-brand">Robert Cory Stine</div>
+                    <div className="navbar-brand">
+                        <Link to="/">Robert Cory Stine</Link>
+                    </div>
                 </div>
                 <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul className="nav navbar-nav">
                         <li className="nav-item">
-                            <a href="#" className="nav-link">About</a>
+                            <Link to="/about" className="nav-link">About</Link>
                         </li>
                         <li className="nav-item">
-                            <a href="#" className="nav-link">Projects</a>
+                            <Link to="/projects" className="nav-link">Projects</Link>
                         </li>
                         <li className="nav-item">
-                            <a href="#" className="nav-link">Skills</a>
+                            <Link to="/skills" className="nav-link">Skills</Link>
                         </li>
                         <li className="nav-item">
-                            <a href="https://rcorystine.medium.com/" className="nav-link">Blog</a>
+                            <Link to="https://rcorystine.medium.com/" className="nav-link">Blog</Link>
                         </li>
                         <li className="nav-item">
-                            <a href="https://www.github.com/GoldenPavilion" className="nav-link">Github</a>
+                            <Link to="https://www.github.com/GoldenPavilion" className="nav-link">Github</Link>
                         </li>
                         <li className="nav-item">
-                           <a href="https://www.linkedin.com/in/robert-cory-stine/" className="nav-link">LinkedIn</a>
+                            <Link to="https://www.linkedin.com/in/robert-cory-stine/" className="nav-link">LinkedIn</Link>
                         </li>
                     </ul>
                 </div>
