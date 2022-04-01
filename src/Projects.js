85c8596b57d222582baabd17865e1158ee890a91
GoldenPavilion <rcorystine@gmail.com>
2022-03-31 22:50:43 -0400

    set up BoardGameShelf card

--- src/Projects.js
+++ src/Projects.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Projects = () => {
     return(
         <div className="projects">
             <div className="card">
-
+                <h5 className="card-title"><Link to="https://github.com/GoldenPavilion/react-redux-boardgames">BoardGameShelf</Link></h5>
+                <p className="card-text">BoardGameShelf is an easy to use tool to help you organize your table top games. Simply sign up for an account, search through our database of games and click "Add To Shelf" when you've found a game you've owned. Pretty soon you'll have a personalized collection of games with details to help you pick which one would be best for your next game night!</p>
             </div>
             <div className="card">
                 
