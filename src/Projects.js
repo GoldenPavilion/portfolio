07e145a4bf2b3bb6ea04035b3aa59a869d7dc7c1
GoldenPavilion <rcorystine@gmail.com>
2022-04-04 20:46:51 -0400

    updated FanMade links

--- src/Projects.js
+++ src/Projects.js
@@ -11,7 +11,8 @@ const Projects = () => {
                 <p className="card-text">BoardGameShelf is an easy to use tool to help you organize your table top games. Simply sign up for an account, search through our database of games and click "Add To Shelf" when you've found a game you've owned. Pretty soon you'll have a personalized collection of games with details to help you pick which one would be best for your next game night!</p>
             </div>
             <div className="card">
-                <h5 className="card-title"><Link to="https://github.com/GoldenPavilion/js_project_fanmade">FanMade</Link></h5>
+                <h3 className="card-title">FanMade</h3>
+                <p><a href="https://github.com/GoldenPavilion/js_project_fanmade">Github</a> | <a href="https://www.youtube.com/watch?v=CnQky8X72a8">Video Demo</a></p>
                 <p className="card-text">A javascript application that curates fanmade merchandise across a variety of sites and services - like Etsy, Teefury and Redbubble.</p>
             </div>
             <div className="card">
