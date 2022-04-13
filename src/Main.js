6f1347576beda726eae39e2f831311ef5ecf56dd
GoldenPavilion <rcorystine@gmail.com>
2022-04-12 21:48:22 -0400

    wrapped img and text into separate divs for organizing

--- src/Main.js
+++ src/Main.js
@@ -7,12 +7,16 @@ const Main = () => {
 
     return(
         <div className="jumbotron">
-            <img src={headshot} alt="A headshot of Cory!" width="200" height="280" />
-            <h1
-                onMouseEnter={() => setTitle("And I Build Web Applications!")}
-                onMouseLeave={() => setTitle("Hi I'm Cory!")}
-            >{ title }</h1>
-            <p>I'm a full stack web developer, a customer service expert, and a big ol' nerd.</p>
+            <div className="jumbo-section">
+                <img src={headshot} alt="A headshot of Cory!" width="200" height="280" />
+            </div>
+            <div className="kumbo-section">
+                <h1
+                    onMouseEnter={() => setTitle("And I Build Web Applications!")}
+                    onMouseLeave={() => setTitle("Hi I'm Cory!")}
+                >{ title }</h1>
+                <p>I'm a full stack web developer, a customer service expert, and a big ol' nerd.</p>
+            </div>
         </div>
     )
 }
