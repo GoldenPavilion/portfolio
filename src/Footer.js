32664221b254452f397ce19f09aef31c9d2cabbd
GoldenPavilion <rcorystine@gmail.com>
2022-04-15 22:09:35 -0400

    added linkedin and github icons

--- src/Footer.js
+++ src/Footer.js
@@ -4,6 +4,8 @@ const Footer = () => {
     return(
         <div className="footer">
             <a href="mailto: rcorystine@gmail.com"><i className="bi bi-envelope"></i></a>
+            <i class="bi bi-linkedin"></i>
+            <i class="bi bi-github"></i>
         </div>
     )
 }
