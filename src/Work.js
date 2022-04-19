16a8af74554bfcc3bf5222ba3fb1474ca47575e3
GoldenPavilion <rcorystine@gmail.com>
2022-04-18 22:04:28 -0400

    added ikea data to work history

--- src/Work.js
+++ src/Work.js
@@ -14,6 +14,17 @@ const Work = () => {
                     <li className="work-list-item">Operated as primary escalation point for challenging customer cases, dispelling conflicts with empathy and consideration - increasing customer satisfaction by 12%. </li>
                 </ul>
             </div>
+            <div className="card">
+                <h3 className="card-title"><a href="www.ikea.com">IKEA Baltimore</a></h3>
+                <h5>Sept. 2011 - Jan. 2015</h5>
+                <h4>Self-Serve Coworker III</h4>
+                <ul className="work-list">
+                    <li className="work-list-item">Assisted customers with complex orders: verifying stock, explaining product details and arranging for delivery.</li>
+                    <li className="work-list-item">Completed the nightly pick list, identifying space for incoming product and pulling items from the warehouse to ensure proper stocking and inventory.</li>
+                    <li className="work-list-item">Built furniture and organized displays to promote specific products and offers.</li>
+                    <li className="work-list-item">Collaborated with co-workers to redesign and renovate the warehouse space.</li>
+                </ul>
+            </div>
         </div>
     )
 }
