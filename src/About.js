0de79c8db9c7792f0d2ed208a0d0610907d52e85
GoldenPavilion <rcorystine@gmail.com>
2022-04-19 22:37:25 -0400

    styled margin and padding of about-text

--- src/About.js
+++ src/About.js
@@ -4,7 +4,7 @@ const About = () => {
     return(
         <div className="about">
             <div className="about-text">
-                <p>Hi I'm Cory Stine! I'm a full stack web developer from Baltimore, Maryland with a passionate devotion to curious exploration, creative problem solving and engaged communication. I love the feeling of diving head first into a subject that challenges my pre-existing notions and pushes my capabilities.</p> 
+                <p><b>Hi I'm Cory Stine!</b> I'm a full stack web developer from Baltimore, Maryland with a passionate devotion to curious exploration, creative problem solving and engaged communication. I love the feeling of diving head first into a subject that challenges my pre-existing notions and pushes my capabilities.</p> 
                 <p>That is why I started my coding journey: to have fun building a few small games and to test my own skill set. But somewhere along the way, I fell in love with programming.</p>
                 <p>Fast forward to two years later and I am a proud graduate of the software engineering program at Flatiron School. I followed my curiosity: learning Javascript, Ruby On Rails, React, Redux and so much more.</p> 
                 <p>There is nothing quite like the endorphin rush I get when a new programming concept really sinks in or when a complex problem is solved with elegant design. Coding engages so many of my core values and interests. I am always looking for new ways to expand my toolset to become a stronger developer.</p> 
