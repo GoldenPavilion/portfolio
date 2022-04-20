e5f125b82fed33d1d4eb94ad73bdb1a4997719ab
GoldenPavilion <rcorystine@gmail.com>
2022-04-19 22:54:34 -0400

    styled jumbotron

--- src/Main.js
+++ src/Main.js
@@ -8,14 +8,18 @@ const Main = () => {
     return(
         <div className="jumbotron">
             <div className="jumbo-section">
-                <img src={headshot} alt="A headshot of Cory!" width="200" height="280" />
+                <div className="headshot">
+                    <img src={headshot} alt="A headshot of Cory!" width="200" height="280" />
+                </div>
             </div>
             <div className="jumbo-section">
-                <h1
-                    onMouseEnter={() => setTitle("And I Build Web Applications!")}
-                    onMouseLeave={() => setTitle("Hi I'm Cory!")}
-                >{ title }</h1>
-                <p>I'm a full stack web developer, a customer service expert, and a big ol' nerd.</p>
+                <div className="jumbo-text">
+                    <h1
+                        onMouseEnter={() => setTitle("And I Build Web Applications!")}
+                        onMouseLeave={() => setTitle("Hi I'm Cory!")}
+                    >{ title }</h1>
+                    <p>I'm a full stack web developer, a customer service expert, and a big ol' nerd.</p>
+                </div>
             </div>
         </div>
     )
